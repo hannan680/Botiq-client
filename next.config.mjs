b534502e50c8839f026c7e13c4a39bf559c197e6
hannan <hannanshoukat55@gmail.com>
2024-10-07 21:17:08 +0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint type checking during the build
+  },
+};
+
+export default nextConfig;
